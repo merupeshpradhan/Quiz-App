481ae4adfb036b11eeda12ff5ad85770e67497c5
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-03-20 16:56:16 +0530

    Adding next question coming, answer list showing level, web question area width and hight setup and my project complet 85% now

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,46 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { data } from "../../Questions/data";
 
 function Quiz() {
+  let [index, setindex] = useState(0);
+  let [currentQuestion, setCurrentQuestion] = useState(data[index]);
+  let [selectedAnswer, setSelectedAnswer] = useState(null);
+  let [isAnswered, setIsAnswered] = useState(false);
+
+  useEffect(() => {
+    setCurrentQuestion(data[index]);
+    setSelectedAnswer(null);
+    setIsAnswered(false);
+  }, [index]);
+
+  const checkAns = (ans) => {
+    if (!isAnswered) {
+      setSelectedAnswer(ans);
+      setIsAnswered(true);
+    }
+  };
+
+  const nextQuestion = () => {
+    if (index < data.length - 1) {
+      setindex(index + 1);
+    }
+  };
+
+  const options = Object.keys(currentQuestion)
+    .filter((key) => key.startsWith("option"))
+    .map((key) => currentQuestion[key]);
+
   return (
     <div className="bg-blue-400 h-screen flex justify-center items-center">
-      <div className="h-[60vh] w-[29vw] bg-blue-200 rounded-xl">
-        <div className="mt-3 px-8">
-          <h2 className="text-blue-600 text-2xl font-medium mb-1 cursor-default">
-            Quiz
-          </h2>
-          <hr className="border-black border-t-2 mt-3" />
-        </div>
+      <div className="p-4 bg-blue-200 rounded-xl m-5">
+        <h2 className="text-blue-600 text-2xl font-medium mb-1 cursor-default">
+          JavaScript Quiz
+        </h2>
+        <hr className="border-black border-t-2 mt-3" />
 
         <div className="mt-5 flex flex-col">
-          <div className="pl-5 ml-3">
+          <div className=" ">
             <h2 className="font-medium text-lg cursor-default">
-              1 . Inside which HTML element do we put the JavaScript?
+              {index + 1} . {currentQuestion.question}
             </h2>
+
             <ul className="mt-5">
-              <li className=" flex items-center font-normal bg-blue-100 border w-[25vw] h-[5vh] mb-5 rounded-md p-2 cursor-pointer">
-                <p>&lt;script&gt;</p>
-              </li>
-              <li className=" flex items-center font-normal bg-blue-100 border w-[25vw] h-[5vh] mb-5 rounded-md p-2 cursor-pointer">
-                <p>&lt;scripting&gt;</p>
-              </li>
-              <li className=" flex items-center font-normal bg-blue-100 border w-[25vw] h-[5vh] mb-5 rounded-md p-2 cursor-pointer">
-                <p>&lt;js&gt;</p>
-              </li>
-              <li className=" flex items-center font-normal bg-blue-100 border w-[25vw] h-[5vh] mb-5 rounded-md p-2 cursor-pointer">
-                <p>&lt;javascript&gt;</p>
-              </li>
+              {options.map((option, i) => {
+                let isCorrect = currentQuestion.ans === i + 1;
+                let isSelected = selectedAnswer === i;
+
+                return (
+                  <li
+                    key={i}
+                    className={`flex items-center font-normal border h-[6vh] mb-5 rounded-md p-2 cursor-pointer 
+                 ${
+                   isAnswered
+                     ? isCorrect
+                       ? "bg-green-500 border-green-500 text-white"
+                       : isSelected
+                       ? "bg-red-500 border-red-500 text-white "
+                       : "bg-blue-100"
+                     : "bg-blue-100"
+                 }`}
+                    onClick={() => checkAns(i)}
+                  >
+                    <p>{option}</p>
+                  </li>
+                );
+              })}
             </ul>
           </div>
 
           <div className="flex justify-center mt-5 ">
-            <button className="border-0 p-[10px] w-[8vw] bg-blue-500 hover:bg-blue-800 rounded-xl text-white cursor-pointer">
-              Submit
+            <button
+              onClick={nextQuestion}
+              className="border-0 p-[10px] w-1/2 bg-blue-500 hover:bg-blue-800 rounded-xl text-white cursor-pointer"
+            >
+              Next
             </button>
           </div>
         </div>
 
         <div className="flex justify-center mt-2 font-serif cursor-default">
-          1 of 20 quistions
+          {index + 1} of {data.length} Question
         </div>
       </div>
     </div>
---
+++ src/Questions/data.js
@@ -0,0 +1,99 @@
+export const data = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    option1: "<script>",
+    option2: "<scripting>",
+    option3: "<js>",
+    option4: "<javascript>",
+    ans: 1,
+  },
+  {
+    question:
+      "What is the correct JavaScript syntax to change the content of the HTML element below? <p id='demo'>This is a demonstration.</p>",
+    option1: " document.getElementByName('p').innerHTML = 'Hello World!';",
+    option2: " document.getElement('p').innerHTML = 'Hello World!';",
+    option3: " #demo.innerHTML = 'Hello World!;",
+    option4: " document.getElementById('demo').innerHTML = 'Hello World!';",
+    ans: 4,
+  },
+  {
+    question:
+      "Where is the correct place to insert a JavaScript?",
+    option1: "Both the <head> section and the <body> section are correct",
+    option2: "The <body> section",
+    option3: "The <head> section",
+    ans: 1,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    option1: "<script src='xxx.js'",
+    option2: "<script name='xxx.js'",
+    option3: "<script href='xxx.js'",
+    ans: 1,
+  },
+  {
+    question:
+      "The external JavaScript file must contain the <script> tag",
+    option1: "False",
+    option2: "True",
+    ans: 1,
+  },
+  {
+    question:
+      "How do you write 'Hello World' in an alert box?",
+    option1: "msgBox('Hello World');",
+    option2: "alertBox('Hello World');",
+    option3: "alert('Hello World');",
+    option4: " msg('Hello World');",
+    ans: 3,
+  },
+  {
+    question:
+      "How do you create a function in JavaScript?",
+    option1: "function = myFunction()",
+    option2: "function:myFunction();",
+    option3: "function myFunction()",
+    ans: 3,
+  },
+  {
+    question:
+      "How do you call a function named 'myFunction'?",
+    option1: "call myFunction()",
+    option2: "myFunction()",
+    option3: "call function myFunction()",
+    ans: 2,
+  },{
+    question:
+      "How to write an IF statement in JavaScript?",
+    option1: "if (i == 5)", 
+    option2: "if i = 5 then",
+    option3: "if i = 5",
+    option3: "if i == 5 then",
+    ans: 1,
+  },{
+    question:
+      "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+    option1: "if (i <> 5)", 
+    option2: "if i <> 5 ",
+    option3: "if (i != 5)",
+    option3: "if i =! 5 then",
+    ans: 1,
+  },{
+    question:
+      "How does a WHILE loop start?",
+    option1: "while (i <= 10)", 
+    option2: "while i = 1 to 10 ",
+    option3: "while (i <= 10; i++)",
+    ans: 1,
+  },
+  {
+    question:
+      "How does a FOR loop start?",
+    option1: "for (i = 0; i <= 5)", 
+    option2: "for i = 1 to 5",
+    option3: "for (i <= 5; i++)",
+    option4: "for (i = 0; i<=5; i++)",
+    ans: 3,
+  },
+];
